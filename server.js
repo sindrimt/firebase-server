50c96292a5b6d72cc6c79ba5c62811ce8b480d6c
sindrimt <sindrimt@gmail.com>
2024-02-17 11:20:49 +0100

    changed to query

--- server.js
+++ server.js
@@ -124,7 +124,7 @@ app.get("/fetchChatDetails", async (req, res) => {
 
 app.get("/fetchChat", async (req, res) => {
     try {
-        const chatID = req.body.chatID;
+        const chatID = req.query.chatID;
         const fundingPipsDataRef = db.collection(collectionName);
         const querySnapshot = await fundingPipsDataRef.where("chatID", "==", chatID).get();
 
